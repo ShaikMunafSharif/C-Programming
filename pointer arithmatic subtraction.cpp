c04f9f1c630dd026cf205a83c29b736ca817d2b8
ShaikMunafSharif <169081792+ShaikMunafSharif@users.noreply.github.com>
2025-08-24 21:59:12 +0530

    Add files via upload

---
+++ pointer arithmatic subtraction.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	int a[] = {2,4,3,0,-7};
+	int *p=a;
+	int *q=&a[3];
+	printf("q-p=%d\n",q-p);
+	printf("p-q=%d\n",p-q);
+	printf("Value=%d\n",*q);
+	q=q-2;
+	printf("Value=%d\n",*q);
+	p=p+2;
+	printf("q-p=%d",q-p);
+	q=q-2;
+	printf("Value %d",*q);
+	return 0;
+}
